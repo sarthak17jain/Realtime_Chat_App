3deda62ee60ac5794a67d2d7d7e4628f2b2ff3c0
Sarthak Jain <77235755+sarthak17jain@users.noreply.github.com>
2023-12-31 18:11:25 +0530

    initial_commit

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+    try {
+        console.log(process.env.DB_LINK);
+        const conn = await mongoose.connect(process.env.DB_LINK);
+
+        console.log(
+            `MongoDB Connected: ${conn.connection.host}`.cyan.underline
+        );
+    } catch (error) {
+        console.error(`Error: ${error.message}`.red.bold);
+        process.exit(1); // Exit with a non-zero status code to indicate an error
+    }
+};
+
+module.exports = connectDB;
---
+++ frontend/src/components/miscellaneous/TypingAnimation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TypingAnimationStyle.css";
+
+const TypingAnimation = () => {
+    return (
+        <div class="chat-bubble">
+            <div class="typing">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            </div>
+        </div>
+    )
+};
+
+export default TypingAnimation;
